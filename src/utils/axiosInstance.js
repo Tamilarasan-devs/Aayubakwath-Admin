23d67ae8bbb52fad4cd43cceeff7f45c152e1cec
git config --global user.name <tamilarasan.softwarengineer@gmail.com>
2026-07-18 15:26:28 +0530

    api

--- src/utils/axiosInstance.js
+++ src/utils/axiosInstance.js
@@ -1,7 +1,8 @@
 import axios from 'axios'
 
 // const BASE_URL = import.meta.env.VITE_API_BASE_URL || "http://localhost:5001"
-const BASE_URL = import.meta.env.VITE_API_BASE_URL || "https://aayubakwath-backend-production-e12c.up.railway.app"
+// const BASE_URL = import.meta.env.VITE_API_BASE_URL || "https://aayubakwath-backend-production-e12c.up.railway.app"
+const BASE_URL = import.meta.env.VITE_API_BASE_URL || "https://aayubakwath-backend-production-2e7f.up.railway.app"
 
 export const axiosInstance = axios.create({
   baseURL: `${BASE_URL}/api/v1`,
